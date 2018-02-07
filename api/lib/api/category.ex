e19fd960ff6b18fcec545a0f9190ecca4271c1dc
jonathanhuang13 <jonathan.huang1@gmail.com>
2018-02-07 05:25:06 -0800

    create categories relation

---
+++ api/lib/api/category.ex
@@ -0,0 +1,20 @@
+defmodule Api.Category do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Api.Category
+
+
+  schema "categories" do
+    field :name, :string
+
+    belongs_to :parent, Category
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Category{} = category, attrs) do
+    category
+    |> cast(attrs, [:name, :parent_id])
+    |> validate_required([:name])
+  end
+end
