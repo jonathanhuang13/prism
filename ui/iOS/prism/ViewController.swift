1051eaedbc905b56b4771ec59ccd81af30875d81
jonathanhuang13 <jonathan.huang1@gmail.com>
2018-03-03 16:17:30 -0800

    start xcode iOS swift UI

---
+++ ui/iOS/prism/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  prism
+//
+//  Created by Jonathan Huang on 3/3/18.
+//  Copyright © 2018 Jonathan Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func tapButton(_ sender: UIButton) {
+        print("tapped button")
+    }
+}
+
